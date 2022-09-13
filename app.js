13e00172f96d19acce08561b0d39823427c19367
SzymonSwiatek <swiatek1996@gmail.com>
2022-09-13 21:44:26 +0200

    update price

--- app.js
+++ app.js
@@ -5,8 +5,11 @@ parent.addEventListener("click", (btn) => {
     btn.target.classList.contains("price-options") &&
     !btn.target.classList.contains("active")
   ) {
-    const ancestor = btn.target.closest(".switch");
+    const ancestor = btn.target.closest(".option");
     ancestor.querySelector(".active").classList.toggle("active");
     btn.target.classList.toggle("active");
+
+    ancestor.querySelector(".one-month").classList.toggle("price-visible");
+    ancestor.querySelector(".six-months").classList.toggle("price-visible");
   }
 });
